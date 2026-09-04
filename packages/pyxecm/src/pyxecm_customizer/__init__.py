953a240410d83e2930099da374195ab61ec39eb0
Release Agent <ci@gitlab.otxlab.net>
2026-09-04 13:43:11 +0000

    Release version: 3.5.2

--- packages/pyxecm/src/pyxecm_customizer/__init__.py
+++ packages/pyxecm/src/pyxecm_customizer/__init__.py
@@ -5,5 +5,3 @@ __copyright__ = "Copyright (C) 2024-2025, OpenText"
 __credits__ = ["Kai-Philip Gatzweiler"]
 __maintainer__ = "Dr. Marc Diefenbruch"
 __email__ = "mdiefenb@opentext.com"
-
-from .__main__ import main as main
